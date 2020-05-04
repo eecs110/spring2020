10962932ecb45d45182b7e1a4389fdbe384bec44
Sarah <vanwars@gmail.com>
2020-05-03 22:16:34 -0500

    Posting L9

---
+++ course-files/lectures/lecture_09/03_invoke_function_via_loops.py
@@ -0,0 +1,15 @@
+# invoke the function you made in #2 to generate a 
+# personalized message to everyone in the list.
+
+people = [
+    ['Tammy Spencer', '42022 Daniel Forest Apt. 201', 'Lake Amyhaven', 'Louisiana', '54479', 'Runner, broadcasting/film/video'],
+    ['Amy Meyer', '5558 Brown Street Apt. 718', 'Calebport', 'Wyoming', '96398', 'Administrator, charities/voluntary organisations'],
+    ['Michael Gutierrez', '560 Rhonda Isle Apt. 851', 'West Dakotaside', 'South Carolina', '92315', 'Location manager'],
+    ['Victoria Zhang', '9007 Rivera Highway Apt. 726', 'Andreafurt', 'Connecticut', '08533', 'Lecturer, further education'],
+    ['Dorothy Acevedo', '43878 Deleon Ranch Apt. 191', 'Emilystad', 'Vermont', '05721', 'Social worker'],
+    ['Richard Haley', '482 Robertson Plaza', 'West Kelly', 'Alabama', '65044', 'Fashion designer'],
+    ['Christopher Jimenez MD', '5679 Pamela Wall Apt. 023', 'West Nathan', 'Delaware', '63181', 'Naval architect'],
+    ['Paul Peterson', '415 Hernandez Islands Suite 379', 'Port Matthew', 'Utah', '99271', 'Publishing rights manager'],
+    ['Christine Lambert DDS', '043 Wright Key Suite 737', 'Port Lisa', 'Vermont', '84484', 'Technical author'],
+    ['Robert Jones', '0816 Tyler Inlet', 'Courtneyberg', 'Nevada', '05095', 'Clinical psychologist'],
+]
--- pages/_lectures/11lecture.md
+++ pages/_lectures/11lecture.md
@@ -1,14 +1,15 @@
 ---
-layout: lecture
+layout: module
 title: | 
     User Inputs & Events
 type: lecture
-draft: 1
+draft: 0
 num: 9
-description:
-  - If / Else
-  - Booleans
 due_date: 2020-05-05
+slides: 
+   - title: "Handling User Input & Event Handlers"
+     url: https://docs.google.com/presentation/d/18EMjTKqVWranGyYn28ps_dfExS-qdBQQ1JJRHiRF4Es/edit?usp=sharing
+exercise_url: "lecture_08.zip"
 # videos:
 #    - title: "A"
 #      url: #
@@ -24,4 +25,4 @@ due_date: 2020-05-05
 #      duration: "0:00"
 ---
 
-TBD
+Today, we will be going over some various ways of interpreting input beyond the command line using event handlers. Nothing from this lecture will be on the exam, but this lecture will certainly help you with [project 1](../assignments/p1) (due in 2 weeks).
